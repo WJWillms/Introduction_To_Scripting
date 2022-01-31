cda4d62d2485cd00c58a8418813e9762ec149d2b
WJWillms <98718411+WJWillms@users.noreply.github.com>
2022-01-30 20:26:16 -0600

    Add files via upload

---
+++ Assign1_Q1.py
@@ -0,0 +1,15 @@
+#Assignment 1
+#Question 1
+#Print Information to Screen
+
+myName = "Wade Willms"
+address = "### Road"
+st = "TX"
+city = "Corpus Christi"
+zip = "78414"
+phoneNumber = "(###) ### - ####"
+myMajor = "Computer Science"
+print(address)
+print(city, st, zip)
+print(phoneNumber)
+print("My Major: ", myMajor)
